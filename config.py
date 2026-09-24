9bb7234b0aec2f10bcc412252980e4bde1babfb2
miko777hh-lgtm <miko777hh@gmail.com>
2026-09-24 10:12:40 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv(override=False)
-
-
-def get_env(key, default=""):
-    return os.environ.get(key, default)
-
-
-BOT_TOKEN = get_env("BOT_TOKEN", "")
-ADMIN_ID = int(get_env("ADMIN_ID", "0") or 0)
-DB_PATH = get_env("DB_PATH", "zara_bot.sqlite3")
-TIMEZONE = get_env("TIMEZONE", "Asia/Tehran")
-
-SCHEDULER_INTERVAL = 20
-BACKUP_INTERVAL = 86400
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "0") or 0)
